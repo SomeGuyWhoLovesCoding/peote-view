f518e4db9cf7d2a4f8d02889740c1301a389c3a1
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2026-03-03 14:58:56 -0500

    Revert "Remove ES3 framebuffer SRGB disabling"
    This reverts commit 1e31e64e5738d2f0369eb02e557c7bcf6fa3ef33.

--- src/peote/view/PeoteView.hx
+++ src/peote/view/PeoteView.hx
@@ -620,6 +620,7 @@ class PeoteView
 		
 		display.renderFramebuffer(this); // <-- render display
 
+		if (PeoteGL.Version.isES3) gl.disable(0x8DB9); // GL_FRAMEBUFFER_SRGB_EXT
 		gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 		//gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null);
 		
